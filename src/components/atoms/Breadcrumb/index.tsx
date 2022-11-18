0a0fa65d935f0a05a62bc8c3e35510b0c827ffe3
thuynt-2588 <nguyen.thi.thuy-d@sun-asterisk.com>
2022-11-18 07:54:36 +0700

    Breadcrumb & input common

---
+++ src/components/atoms/Breadcrumb/index.tsx
@@ -0,0 +1,31 @@
+import clsx from "clsx";
+import React from "react";
+import { BreadcrumbValue } from "../../../types/common";
+import "./index.scss";
+
+type Props = {
+  items?: BreadcrumbValue[];
+};
+
+const Breadcrumb: React.FC<Props> = ({ items }) => {
+  return (
+    <section className="breadcrumb">
+      <div className="container">
+        <ul className="breadcrumb__ul">
+          {items?.map(({ label, key, url }) => (
+            <li key={key} className="breadcrumb__li">
+              <a
+                {...(url ? { href: url } : {})}
+                className={clsx(url ? "breadcrumb__link" : "breadcrumb__link-not")}
+              >
+                {label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Breadcrumb;
---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { clsx } from "clsx";
+import "./index.scss";
+
+type Props = {
+  value: string;
+  label: string;
+  required: boolean;
+  name: string;
+  className?: string;
+  placeholder: string;
+  type: string;
+  onChange: (e: any) => void;
+  errors?: string[];
+};
+
+export const Input: React.FC<Props> = ({
+  value,
+  label,
+  required,
+  name,
+  className,
+  placeholder,
+  type,
+  onChange,
+  errors,
+}) => {
+  return (
+    <div className="form-group">
+      {label && (
+        <label htmlFor="input-field">
+          {label}
+          <span className={clsx(required || false)}>*</span>
+        </label>
+      )}
+      <input
+        type={type}
+        value={value}
+        name={name}
+        className={className}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+
+      <span className="error">{errors}</span>
+    </div>
+  );
+};
+
+export default Input;
