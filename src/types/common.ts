a46eb8d4099dfbe79a23a65efc1379ae41877cbd
thuynt-2588 <nguyen.thi.thuy-d@sun-asterisk.com>
2022-11-18 07:55:30 +0700

    Feature login

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,1 +1,2 @@
-export type BreadcrumbValue = { label: string; key: string | number, url: string; };
+export type BreadcrumbValue = { label: string; key: string | number; url: string; };
+export type TabValue = { id: string | number; title: string; url: string; };
